0690c1ef55416cb32c8323bf69eaf21b27ac71e1
Mitutoyum <mitutoyum@proton.me>
2026-09-06 14:11:23 +0700

    fix: handle bot._client.chat error

--- src/commands/chat.js
+++ src/commands/chat.js
@@ -4,21 +4,25 @@ export default {
     name: 'chat',
     description: 'Make bot send message',
     execute(args, context) {
-        const [bot_name, ...message_args] = args;
+        const [name, ...message_args] = args;
         const message = message_args.join(' ');
 
-        if (!bot_name || !message) {
+        if (!name || !message) {
             logger.log('Usage: chat <bot> <message>');
             return;
         }
 
-        const bot = context.state.bots[bot_name];
+        const bot = context.state.bots[name];
 
         if (!bot) {
-            logger.log(`No bot found with the given name: ${bot_name}`);
+            logger.log(`No bot found with the given name: ${name}`);
             return;
         }
 
-        bot.chat(message);
+        try {
+            bot.chat(message);
+        } catch (err) {
+            logger.log(`⚠️  ${name} failed to send chat: ${err.message}`);
+        }
     }
 }
